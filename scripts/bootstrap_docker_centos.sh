63b3a054a3a960c513fcd729eb38eb786e058090
Lampros Oikonomou <Lampros.OIKONOMOU@intrasoft-intl.com>
2018-09-02 21:00:58 +0300

    Install docker from default repos because docker-ce-17.03.3.ce-1.el7 does not install

--- scripts/bootstrap_docker_centos.sh
+++ scripts/bootstrap_docker_centos.sh
@@ -6,18 +6,18 @@ set -e
 
 echo "Installing Docker..."
 ## Install required packages. yum-utils provides the yum-config-manager utility, and device-mapper-persistent-data and lvm2 are required by the devicemapper storage driver.
-sudo yum install -y yum-utils
-#sudo yum install -y device-mapper-persistent-data lvm2
+#sudo yum install -y yum-utils
+#-sudo yum install -y device-mapper-persistent-data lvm2
 
 ## Use the following command to set up the stable repository. You always need the stable repository, even if you want to install builds from the edge or test repositories as well.
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 ## Optional: Enable the edge and test repositories. These repositories are included in the docker.repo file above but are disabled by default. You can enable them alongside the stable repository.
-# sudo yum-config-manager --enable docker-ce-edge
-# sudo yum-config-manager --enable docker-ce-test
+#-sudo yum-config-manager --enable docker-ce-edge
+#-sudo yum-config-manager --enable docker-ce-test
 
 ##You can disable the edge or test repository by running the yum-config-manager command with the --disable flag. To re-enable it, use the --enable flag. The following command disables the edge repository.
-# sudo yum-config-manager --disable docker-ce-edge
+#-sudo yum-config-manager --disable docker-ce-edge
 
 ## Install the latest version of Docker CE, or go to the next step to install a specific version:
 #sudo yum install -y docker-ce
@@ -28,7 +28,10 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 ## docker-ce.x86_64            18.03.0.ce-1.el7.centos             docker-ce-stable
 ## The list returned depends on which repositories are enabled, and is specific to your version of CentOS (indicated by the .el7 suffix in this example).
 ## b. Install a specific version by its fully qualified package name, which is the package name (docker-ce) plus the version string (2nd column) up to the first hyphen, separated by a hyphen (-), for example, docker-ce-18.03.0.ce.
-sudo yum install docker-ce-17.03.3.ce-1.el7
+#-sudo yum install docker-ce-17.03.3.ce-1.el7
+
+########### Alternativelly install via default repositories because docker-ce-17.03.3.ce-1.el7 does not install
+sudo yum install -y docker
 
 ## Start and enable Docker.
 echo "Start and enable Docker..."
