8dc339b030031cfca43bb404223630728f4c5e10
Lampros Oikonomou <Lampros.OIKONOMOU@intrasoft-intl.com>
2018-09-03 19:10:58 +0300

    Made script running on both ubuntu and centos

--- scripts/bootstrap_docker.sh
+++ scripts/bootstrap_docker.sh
@@ -3,49 +3,58 @@
 set -e
 
 # See: https://docs.docker.com/install/linux/docker-ce/centos/
+# To check if ubuntu or centos see: https://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
+
+SCR_OS=$(awk -F= '/^NAME/{print toupper($2)}' /etc/os-release | tr -d \")
+echo "OS script is running is: $SCR_OS"
 
 echo "Installing Docker..."
 
-#################################################
-################ UBUNTU
-#################################################
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+if [ "$SCR_OS" == "UBUNTU" ]
+then
+  #################################################
+  ################ UBUNTU
+  #################################################
+  apt-get update
+  apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+  apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+else
 
-#################################################
-################ CENTOS
-#################################################
+  #################################################
+  ################ CENTOS
+  #################################################
 
-## Install required packages. yum-utils provides the yum-config-manager utility, and device-mapper-persistent-data and lvm2 are required by the devicemapper storage driver.
-#sudo yum install -y yum-utils
-#-sudo yum install -y device-mapper-persistent-data lvm2
+  ## Install required packages. yum-utils provides the yum-config-manager utility, and device-mapper-persistent-data and lvm2 are required by the devicemapper storage driver.
+  sudo yum install -y yum-utils
+  sudo yum install -y device-mapper-persistent-data lvm2
 
-## Use the following command to set up the stable repository. You always need the stable repository, even if you want to install builds from the edge or test repositories as well.
-#sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  ## Use the following command to set up the stable repository. You always need the stable repository, even if you want to install builds from the edge or test repositories as well.
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-## Optional: Enable the edge and test repositories. These repositories are included in the docker.repo file above but are disabled by default. You can enable them alongside the stable repository.
-#-sudo yum-config-manager --enable docker-ce-edge
-#-sudo yum-config-manager --enable docker-ce-test
+  ## Optional: Enable the edge and test repositories. These repositories are included in the docker.repo file above but are disabled by default. You can enable them alongside the stable repository.
+  #-sudo yum-config-manager --enable docker-ce-edge
+  #-sudo yum-config-manager --enable docker-ce-test
 
-##You can disable the edge or test repository by running the yum-config-manager command with the --disable flag. To re-enable it, use the --enable flag. The following command disables the edge repository.
-#-sudo yum-config-manager --disable docker-ce-edge
+  ##You can disable the edge or test repository by running the yum-config-manager command with the --disable flag. To re-enable it, use the --enable flag. The following command disables the edge repository.
+  sudo yum-config-manager --disable docker-ce-edge
+  sudo yum-config-manager --disable docker-ce-test
 
-## Install the latest version of Docker CE, or go to the next step to install a specific version:
-#sudo yum install -y docker-ce
+  ## Install the latest version of Docker CE, or go to the next step to install a specific version:
+  sudo yum install -y docker-ce
 
-## To install a specific version of Docker CE, list the available versions in the repo, then select and install:
-## a. List and sort the versions available in your repo. This example sorts results by version number, highest to lowest, and is truncated:
-## yum list docker-ce --showduplicates | sort -r
-## docker-ce.x86_64            18.03.0.ce-1.el7.centos             docker-ce-stable
-## The list returned depends on which repositories are enabled, and is specific to your version of CentOS (indicated by the .el7 suffix in this example).
-## b. Install a specific version by its fully qualified package name, which is the package name (docker-ce) plus the version string (2nd column) up to the first hyphen, separated by a hyphen (-), for example, docker-ce-18.03.0.ce.
-#-sudo yum install docker-ce-17.03.3.ce-1.el7
+  ## To install a specific version of Docker CE, list the available versions in the repo, then select and install:
+  ## a. List and sort the versions available in your repo. This example sorts results by version number, highest to lowest, and is truncated:
+  ## yum list docker-ce --showduplicates | sort -r
+  ## docker-ce.x86_64            18.03.0.ce-1.el7.centos             docker-ce-stable
+  ## The list returned depends on which repositories are enabled, and is specific to your version of CentOS (indicated by the .el7 suffix in this example).
+  ## b. Install a specific version by its fully qualified package name, which is the package name (docker-ce) plus the version string (2nd column) up to the first hyphen, separated by a hyphen (-), for example, docker-ce-18.03.0.ce.
+  #-sudo yum install docker-ce-17.03.3.ce-1.el7
 
-########### Alternativelly install via default repositories because docker-ce-17.03.3.ce-1.el7 does not install
-#sudo yum install -y docker
+  ########### Alternativelly install via default repositories because docker-ce-17.03.3.ce-1.el7 does not install
+  #sudo yum install -y docker
+fi
 
 #################################################
 ################ BOTH
--- scripts/bootstrap_kubernetes_kubeadmn.sh
+++ scripts/bootstrap_kubernetes_kubeadmn.sh
@@ -1,50 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 # See: https://kubernetes.io/docs/setup/independent/install-kubeadm/
+# To check if ubuntu or centos see: https://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
+
+SCR_OS=$(awk -F= '/^NAME/{print toupper($2)}' /etc/os-release | tr -d \")
+echo "OS script is running is: $SCR_OS"
 
 ## Install kubeadmn, kubelet, kubectl
 echo "Installing kubeadmn, kubelet, kubectl..."
 
-#################################################
-################ UBUNTU
-#################################################
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+if [ "$SCR_OS" = "UBUNTU" ]
+then
+  #################################################
+  ################ UBUNTU
+  #################################################
+  apt-get update && apt-get install -y apt-transport-https curl
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+  apt-get update
+  apt-get install -y kubelet kubeadm kubectl
+  apt-mark hold kubelet kubeadm kubectl
 
+else
+  #################################################
+  ################ CENTOS
+  #################################################
 
-#################################################
-################ CENTOS
-#################################################
-
-#cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-#[kubernetes]
-#name=Kubernetes
-#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-#enabled=1
-#gpgcheck=1
-#repo_gpgcheck=1
-#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-#exclude=kube*
-#EOF
-#setenforce 0
-#yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-#systemctl enable kubelet && systemctl start kubelet
-#
-### Some users on RHEL/CentOS 7 have reported issues with traffic being routed incorrectly due to iptables being bypassed. You should ensure net.bridge.bridge-nf-call-iptables is set to 1 in your sysctl config, e.g.
-#cat <<EOF >  /etc/sysctl.d/k8s.conf
-#net.bridge.bridge-nf-call-ip6tables = 1
-#net.bridge.bridge-nf-call-iptables = 1
-#EOF
-#sysctl --system
-#
+  cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+  setenforce 0
+  yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+  systemctl enable kubelet && systemctl start kubelet
+  
+  ## Some users on RHEL/CentOS 7 have reported issues with traffic being routed incorrectly due to iptables being bypassed. You should ensure net.bridge.bridge-nf-call-iptables is set to 1 in your sysctl config, e.g.
+  cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+  sysctl --system
+  
+fi
 
 #################################################
 ################ BOTH
